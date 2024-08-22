bbc05b0a6c08d8b061e5e148dec043d819f25a0f
MaherWasel <132270545+MaherWasel@users.noreply.github.com>
2024-08-22 22:04:23 +0300

    diaries controller added

--- diaries_mobile/lib/modules/home/data/home_repo.dart
+++ diaries_mobile/lib/modules/home/data/home_repo.dart
@@ -1,1 +1,21 @@
-class HomeRepository {}
+import 'package:diaries_mobile/errors/backend_responses.dart';
+import 'package:diaries_mobile/modules/home/domain/diaries.dart';
+import 'package:either_dart/either.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class HomeRepository {
+  Future<Either<BackEndFailure, BackEndSuccess<List<Diaries>>>> fetchAllDiaries(
+      {required String userId}) async {
+    try {
+      final response = await Supabase.instance.client
+          .from("diares")
+          .select()
+          .eq("user_id", userId);
+      List<Diaries> data = [];
+      data = response.map((e) => Diaries.fromMap(e)).toList();
+      return Right(BackEndSuccess<List<Diaries>>(data: data));
+    } catch (e) {
+      return Left(BackEndFailure());
+    }
+  }
+}
---
+++ diaries_mobile/lib/modules/home/presentation/provider/diaries_controller.dart
@@ -0,0 +1,30 @@
+import 'package:diaries_mobile/modules/home/data/home_repo.dart';
+import 'package:diaries_mobile/modules/home/domain/diaries.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+abstract class HomeStates {}
+
+class HomeSuccess<T> extends HomeStates {
+  final T? data;
+  HomeSuccess({this.data});
+}
+
+class HomeInitial extends HomeStates {}
+
+class HomeLoading extends HomeStates {}
+
+class HomeFailure extends HomeStates {
+  final int? code;
+  final String? errorMessage;
+  HomeFailure({this.code, this.errorMessage});
+}
+
+class DiariesState extends StateNotifier<HomeStates> {
+  DiariesState() : super(HomeInitial());
+  Future<void> loadDiaires({required String userId}) async {
+    state = HomeLoading();
+    final response = await HomeRepository().fetchAllDiaries(userId: userId);
+    response.fold((fail) => state = HomeFailure(),
+        (right) => state = HomeSuccess<List<Diaries>>(data: right.data));
+  }
+}
